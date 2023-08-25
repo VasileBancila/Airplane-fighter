bf77fa76fd0ce6216abba4c229ef800d06e1cb10
Vasile Bancila <bancilavasile93@gmail.com>
2023-08-26 00:01:23 +0200

    adding the final version of the project

--- script.js
+++ script.js
@@ -1,96 +1,149 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-let ox = 150, oy = 350, sizeObj = 50;
-let oxRocket = 0, oyRocket = 0, rockets = [], noCol = 8, startingRokets = 20;
-let score = 0, second = 1;
+let oxAirplane = 150, oyAirplane = 350, sizeObj = 50;
+let boardCol = 8;
+let rockets = [], startingRokets = 20;
+let bullets = [], bulletNumber = 0;
+let intervalTime = 0;
+let gameStats = { time: 0, avoidedRockets: 0, destroyedRockets: 0 };
+let activePlayer = true;
 
 function drawAirplane() {
-	let img = new Image();
-	img.src = "image/f16.png";
+	let imgAirplane = new Image();
+	imgAirplane.src = "image/f16.png";
 	ctx.beginPath();
-	img.addEventListener("load", function () {
-		ctx.drawImage(img, ox, oy, sizeObj, sizeObj);
+	imgAirplane.addEventListener("load", function () {
+		ctx.drawImage(imgAirplane, oxAirplane, oyAirplane, sizeObj, sizeObj);
 	}, false);
 	ctx.closePath();
 }
 
-function drawRockets() { 
+function drawRockets() {
 	let imgRocket = new Image();
 	imgRocket.src = "image/rocket.png";
 	ctx.beginPath();
 	for (let i = 0; i < rockets.length; ++i) {
 		ctx.clearRect(rockets[i].x, rockets[i].y, sizeObj, sizeObj);
-		ctx.drawImage(imgRocket, rockets[i].x, rockets[i].y, sizeObj, sizeObj); 
-		if (rockets[i].y === 400) { 
+		ctx.drawImage(imgRocket, rockets[i].x, rockets[i].y, sizeObj, sizeObj);
+		if (rockets[i].y === 400) {
 			generateOrReplaceRockets(i); //replace the rockets
+			updateStats("avoidedRockets");
 		}
-		rockets[i].y += 1;
-		if (checkCollision(rockets[i].x, rockets[i].y)) {
+		++rockets[i].y;
+		if (checkCollision(rockets[i].x, rockets[i].y, oxAirplane, oyAirplane)) {
 			gameOver();
 		}
 	}
-	ctx.closePath(); 
+	ctx.closePath();
 }
 
-function drawExplosion() {
-	let img = new Image();
-	img.src = "image/explosion.png";
+function generateOrReplaceRockets(position = -1) {
+	let random = () => Math.floor(Math.random() * boardCol);
+	let x, y, overlap;
+	do {
+		x = random(), y = random();
+		x *= sizeObj, y *= sizeObj; //multiply to get the real position on the canvas
+		y = -y; //we negate y to create the rockets above the canvas
+		overlap = false;
+		for (let i = 0; i < rockets.length; ++i) {
+			if (checkCollision(x, y, rockets[i].x, rockets[i].y)) {
+				overlap = true; //found an overlap
+				break;
+			}
+		}
+	} while (overlap); //repeat until there is no overlap
+	if (position === -1) { // if no position is specified, a new missile is generated
+		rockets.push({ x, y });
+	} else { //replace the rocket at the given position
+		rockets[position] = { x, y };
+	}
+}
+
+function airplaneSkills(ability) {
+	ctx.clearRect(oxAirplane, oyAirplane, sizeObj, sizeObj);
+	drawAirplane();
+	if (ability.code == "ArrowLeft") {
+		oxAirplane -= sizeObj;
+		if (oxAirplane < 0) {
+			oxAirplane = 0;
+		}
+	} else if (ability.code == "ArrowRight") {
+		oxAirplane += sizeObj;
+		if (oxAirplane > canvas.width - sizeObj) {
+			oxAirplane = canvas.width - sizeObj;
+		}
+	} else if (ability.code == "Space") {
+		shootingBullets();
+	}
+}
+
+function drawBullets() {
 	ctx.beginPath();
-	img.addEventListener("load", function () {
-		ctx.drawImage(img, ox - sizeObj / 2, oy - sizeObj, sizeObj + sizeObj, sizeObj + sizeObj);
+	let imgBullet = new Image();
+	imgBullet.src = "image/missile-airplane.png";
+	imgBullet.addEventListener("load", function () {
+		for (let i = 0; i < bulletNumber; ++i) {
+			ctx.clearRect(bullets[i].x, bullets[i].y, sizeObj, sizeObj);
+			ctx.drawImage(imgBullet, bullets[i].x, bullets[i].y, sizeObj, sizeObj);
+			--bullets[i].y;
+			for (let j = 0; j < rockets.length; ++j) {
+				if (checkCollision(rockets[j].x, rockets[j].y, bullets[i].x, bullets[i].y)) {
+					drawExplosion(bullets[i].x, bullets[i].y);
+					rockets[j] = [];
+					bullets[i] = [];
+					generateOrReplaceRockets(j);
+					updateStats("destroyedRockets");
+				}
+			}
+		}
 	}, false);
 	ctx.closePath();
 }
 
-function generateOrReplaceRockets(position = -1) {  //generate or replace a rockets
-	let random = () => Math.floor(Math.random() * noCol);
-	let x = random(), y = random();
-	x *= sizeObj, y *= sizeObj; //multiply to get the real position on the canvas
-	y = -y; //we negate y to create the rockets above the canvas
-	if (position === -1) { // if no position is specified, a new missile is generated
-		rockets.push({ x, y }); 
-	} else { //replace the rocket at the given position
-		rockets[position] = { x, y }; 
+function shootingBullets() {
+	let maxBullets = 100;
+	bullets[bulletNumber] = ({ x: oxAirplane, y: oyAirplane - sizeObj }); //bullets position in front of the plane
+	drawBullets();
+	if (bulletNumber <= maxBullets) {
+		++bulletNumber;
+	} else {
+		document.getElementById("withoutBullets").innerText = "Without bullets / ";
 	}
 }
 
-function moveAirplane(movement) {
-	ctx.clearRect(ox, oy, sizeObj, sizeObj);
-	drawAirplane();
-	if (movement.code == "ArrowLeft") {
-		ox -= sizeObj;
-		if (ox < 0) {
-			ox = 0;
-		}
-	} else if (movement.code == "ArrowRight") {
-		ox += sizeObj;
-		if (ox > canvas.width - sizeObj) {
-			ox = canvas.width - sizeObj;
-		}
-	}
-}
-
-function rocketsAnimation() {
+function animation() {
 	drawRockets();
-	window.requestAnimationFrame(rocketsAnimation);
+	drawBullets();
+	window.requestAnimationFrame(animation);
 }
 
-function checkCollision(posX, posY) {
-	let overlapX = (posX < ox + sizeObj) && (posX + sizeObj > ox);
-	let overlapY = (posY < oy + sizeObj) && (posY + sizeObj > oy);
+function checkCollision(ox1, oy1, ox2, oy2) {
+	let overlapX = (ox1 < ox2 + sizeObj) && (ox1 + sizeObj > ox2);
+	let overlapY = (oy1 < oy2 + sizeObj) && (oy1 + sizeObj > oy2);
 	return overlapX && overlapY;
+}
+
+function drawExplosion(ox, oy) {
+	let imgExplosion = new Image();
+	imgExplosion.src = "image/explosion.png";
+	ctx.beginPath();
+	imgExplosion.addEventListener("load", function () {
+		ctx.drawImage(imgExplosion, ox, oy - sizeObj, sizeObj, sizeObj * 2);
+	}, false);
+	ctx.closePath();
+}
+
+function updateStats(element) {
+	++gameStats[element];
+	document.getElementById(element).innerHTML = gameStats[element];
 }
 
 function gameOver() {
 	document.getElementById("gameOver").innerText = "Game over / ";
-	rockets = [];
-	second = 0;
-	drawExplosion();
-}
-
-function updateScore() {
-	score += second;
-	document.getElementById("score").innerText = score;
+	rockets = [], bullets = [];
+	drawExplosion(oxAirplane, oyAirplane);
+	clearInterval(intervalTime);
+	activePlayer = false;
 }
 
 function reload() { //new game
@@ -99,13 +152,17 @@ function reload() { //new game
 
 function startGame() {
 	drawAirplane();
-	rocketsAnimation();
-	document.addEventListener("keydown", function (movement) {
-		moveAirplane(movement);
+	document.addEventListener("keydown", function (ability) {
+		if (activePlayer) {
+			airplaneSkills(ability);
+		}
 	});
-	for (let i = 0; i < startingRokets; ++i) { 
-		generateOrReplaceRockets();
+	for (let i = 0; i < startingRokets; ++i) {
+		generateOrReplaceRockets(); //generate starting rockets
 	}
-	setInterval(updateScore, 1000);
+	animation();
+	intervalTime = setInterval(function () {
+		updateStats("time");
+	}, 1000);
 	document.getElementById("startGame").disabled = true;
 }
